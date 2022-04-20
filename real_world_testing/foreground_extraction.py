bae8f1c06e23a17e05d3253a17bce9755b3c3e1a
ZjTan4 <ZTAN4@UALBERTA.CA>
2022-04-20 14:47:37 -0600

    Update foreground_extraction.py

--- real_world_testing/foreground_extraction.py
+++ real_world_testing/foreground_extraction.py
@@ -11,15 +11,17 @@ import time
 
 def decode_segmap(image, nc=21):
 
-    label_colors = np.array([(0, 0, 0),  # 0=background
-                           # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
-               (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
-               # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
-               (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
-               # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
-               (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
-               # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
-               (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
+    label_colors = np.array([
+        (0, 0, 0),  # 0=background
+        # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
+        (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
+        # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
+        (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
+        # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
+        (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
+        # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+        (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)
+    ])
 
     r = np.zeros_like(image).astype(np.uint8)
     g = np.zeros_like(image).astype(np.uint8)
@@ -37,15 +39,17 @@ def decode_segmap(image, nc=21):
 
 
 def crop(image, source, nc=21):
-    label_colors = np.array([(0, 0, 0),  # 0=background
-                           # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
-               (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
-               # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
-               (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
-               # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
-               (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
-               # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
-               (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
+    label_colors = np.array([
+        (0, 0, 0),  # 0=background
+        # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
+        (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
+        # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
+        (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
+        # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
+        (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
+        # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+        (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)
+    ])
 
     r = np.zeros_like(image).astype(np.uint8)
     g = np.zeros_like(image).astype(np.uint8)
@@ -85,17 +89,16 @@ def crop(image, source, nc=21):
 def modify():
     ''' Use the below commented code to upload an image using colab.upload in colab notebook but then 
     there is no need to use img parameter and source parameter while using crop function'''
+    plt.imshow(img) 
+    plt.axis('off') 
+    plt.show()
+    trf = T.Compose([
+        T.Resize(640),
+        T.ToTensor(),
+        T.Normalize(mean = [0.485, 0.456, 0.406], 
+        std = [0.229, 0.224, 0.225])
+    ])
 
-    # uploaded=files.upload()
-    # for fn in uploaded.keys():
-    #   path='/content/'+fn
-    #   print('User uploaded file "{name}" with length {length} bytes'.format(name=fn, length=len(uploaded[fn])))
-    #   img = Image.open(path)
-    plt.imshow(img); plt.axis('off'); plt.show()
-    trf = T.Compose([T.Resize(640),
-                   T.ToTensor(),
-                     T.Normalize(mean = [0.485, 0.456, 0.406], 
-                                 std = [0.229, 0.224, 0.225])])
     inp = trf(img).unsqueeze(0)
     out = dlab(inp)['out']
     om = torch.argmax(out.squeeze(), dim=0).detach().cpu().numpy()
@@ -105,8 +108,11 @@ def modify():
 dlab = models.segmentation.deeplabv3_resnet101(pretrained=1).eval()
 
 
-img = Image.open('download.png')
-plt.imshow(img); plt.axis('off'); plt.show()
+img = Image.open('./real_world_testing/download.png')
+img = Image.open('./real_world_testing/00012.jpg')
+# plt.imshow(img)
+# plt.axis('off')
+# plt.show()
 modify()
 
 
